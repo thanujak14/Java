8667af0d083f9516d6c956758f1d12c882065fa2
thanujak14 <thanuja.kaparaboina@gmail.com>
2024-07-31 12:05:13 +0530

    Java files

---
+++ Evenorodd.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+	public static void main(String[] args){
+		int n=123;
+        	int a=n%10;
+        	if(a%2==0)
+        	System.out.println("Even");
+        	else
+        	System.out.println("Odd");
+	}
+}
