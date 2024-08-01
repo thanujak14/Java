d238996dc737c5bda41d97d2ad0a48f68a160c55
thanujak14 <thanuja.kaparaboina@gmail.com>
2024-08-01 11:27:50 +0530

    Day 1

--- day.java
+++ day.java
@@ -2,19 +2,19 @@ class HelloWorld{
 	public static void main(String[] args){
 		int n=1;
 		if(n==1)
-		System.out.println("Sunday");
+		System.out.println("Sun");
 		else if(n==2)
-		System.out.println("Monday");
+		System.out.println("Mon");
 		else if(n==3)
-		System.out.println("Tuesday");
+		System.out.println("Tues");
 		else if(n==4)
-		System.out.println("Wednesday");
+		System.out.println("Wednes");
 		else if(n==5)
-		System.out.println("Thursday");
+		System.out.println("Thurs");
 		else if(n==6)
-		System.out.println("Friday");
+		System.out.println("Fri");
 		else if(n==7)
-		System.out.println("Saturday");
+		System.out.println("Satur");
 		else
 		System.out.println("Invalid input");
 	}
